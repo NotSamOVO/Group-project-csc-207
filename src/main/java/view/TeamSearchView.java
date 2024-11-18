b9b0fddebf0ee47bc24c1f4b03acaf5408334ec3
Christopher Deng <chrisdeng@gmail.com>
2024-11-17 20:54:21 -0500

    Added relevant files in accordance to the Lab 5 template

--- src/main/java/view/TeamSearchView.java
+++ src/main/java/view/TeamSearchView.java
@@ -1,4 +1,26 @@
 package view;
 
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+/**
+ *  The View for the Team Search and Selection Use Case.
+ */
 public class TeamSearchView {
+    private final String viewName = "Team Search";
+
+
 }

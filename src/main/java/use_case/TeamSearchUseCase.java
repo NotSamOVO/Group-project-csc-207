017ddc384f89ae33634f43a47ddb4a234e9376a7
Sam <tianle.xu@mail.utoronto.ca>
2024-11-18 20:53:01 -0500

    tianle.xu added in the usecase for TeamSearchUseCase

--- src/main/java/use_case/TeamSearchUseCase.java
+++ src/main/java/use_case/TeamSearchUseCase.java
@@ -1,4 +1,29 @@
 package use_case;
 
+import api.NFLTeamDataBase;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class TeamSearchUseCase {
+    private NFLTeamDataBase teamDatabase = new NFLTeamDataBase();
+
+    public TeamSearchUseCase(NFLTeamDataBase dataBase) {
+        this.teamDatabase = dataBase;
+    }
+
+    /**
+     * get the team id by inputting team name
+     * @param teamName The name of the team.
+     * @return the id of the team. -1 if the team is not found.
+     */
+    public int getTeamId(String teamName) {
+        JSONArray allTeams = teamDatabase.getAllTeams();
+        for (int i = 0; i < allTeams.length(); i++) {
+            JSONObject team = allTeams.getJSONObject(i);
+            if (team.getString("name").equals(teamName)) {
+                return team.getInt("id");
+            }
+        }
+        return -1;
+    }
 }

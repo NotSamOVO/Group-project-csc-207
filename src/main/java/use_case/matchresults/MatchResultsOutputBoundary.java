4fc64fcc8dad8ee25561143d7170e4d85d71ea0a
vietng04 <viethoang.nguyen@mail.utoronto.ca>
2024-11-22 19:55:51 -0500

    update MatchResultsOutputBoundary

--- src/main/java/use_case/matchresults/MatchResultsOutputBoundary.java
+++ src/main/java/use_case/matchresults/MatchResultsOutputBoundary.java
@@ -1,2 +1,12 @@
-package use_case.matchresults;public interface MatchresultsOutputBoundary {
+package use_case.matchresults;
+
+/**
+ * Output Boundary for actions which are related to match results.
+ */
+public interface MatchResultsOutputBoundary {
+
+    /**
+     * Executes the switch to team search use case.
+     */
+    void switchToTeamSearchView();
 }

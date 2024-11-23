1cb5d1c781a5aee00b794f0b82ded8da1932437f
vietng04 <viethoang.nguyen@mail.utoronto.ca>
2024-11-23 18:48:19 -0500

    update Application

--- src/main/java/app/Config.java
+++ src/main/java/app/Config.java
@@ -1,2 +1,26 @@
-package app;public class Config {
+package app;
+
+import api.NFLTeamDataBase;
+import api.NFLDataBase;
+import use_case.matchresults.MatchResultsUseCase;
+import use_case.teamsearch.TeamSearchUseCase;
+
+public class Config {
+    private final NFLTeamDataBase dataBase = new NFLTeamDataBase();
+
+    /**
+     * Get the TeamSearchUseCase object.
+     * @return TeamSearchUseCase object.
+     */
+    public TeamSearchUseCase getTeamSearchUseCase() {
+        return new TeamSearchUseCase(dataBase);
+    }
+
+    /**
+     * Get the MatchResults object.
+     * @return MatchResults object.
+     */
+    public MatchResultsUseCase getMatchResultsUseCase() {
+        return new MatchResultsUseCase(dataBase);
+    }
 }

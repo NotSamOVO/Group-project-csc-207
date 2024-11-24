36265c0978090936d0f7509f5352f80389708117
vietng04 <viethoang.nguyen@mail.utoronto.ca>
2024-11-24 12:34:36 -0500

    TeamSearchUseCase

--- src/main/java/use_case/teamsearch/TeamSearchUseCase.java
+++ src/main/java/use_case/teamsearch/TeamSearchUseCase.java
@@ -1,2 +1,35 @@
-package use_case.teamsearch;public class TeamSearchUseCase {
+
+package use_case.teamsearch;
+
+import api.NFLTeamDataBase;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import entity.Team;
+import entity.Player;
+
+import java.util.ArrayList;
+
+public class TeamSearchUseCase {
+    private NFLTeamDataBase teamDatabase = new NFLTeamDataBase();
+
+    public TeamSearchUseCase(NFLTeamDataBase dataBase) {
+        this.teamDatabase = dataBase;
+    }
+
+    /**
+     * get the team id by inputting team name
+     * @param teamName The name of the team.
+     * @return the id of the team. -1 if the team is not found.
+     */
+    public int getTeamId(String teamName) {
+        ArrayList<Team> allTeams = teamDatabase.getAllTeams();
+        for (int i = 0; i < allTeams.size(); i++) {
+            Team team = allTeams.get(i);
+            if (team.getName().equals(teamName)) {
+                return team.getId();
+            }
+        }
+        return -1;
+    }
 }

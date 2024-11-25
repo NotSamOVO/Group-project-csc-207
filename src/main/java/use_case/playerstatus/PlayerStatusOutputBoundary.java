ab868cd94770c2880e00a8bcd93cf18e1a6b9d2a
Sam <tianle.xu@mail.utoronto.ca>
2024-11-25 11:40:16 -0500

    tianle.xu edditing errors

--- src/main/java/use_case/playerstatus/PlayerStatusOutputBoundary.java
+++ src/main/java/use_case/playerstatus/PlayerStatusOutputBoundary.java
@@ -4,11 +4,6 @@ package use_case.playerstatus;
  * The output boundary for the PlayerStatus Use Case.
  */
 public interface PlayerStatusOutputBoundary {
-
-    void presentPlayerStatus(PlayerStatusOutputData outputData);
-
-    void presentPlayerNotFound(String errorMessage);
-
     /**
      * Switches to the Teamsearch View.
      */

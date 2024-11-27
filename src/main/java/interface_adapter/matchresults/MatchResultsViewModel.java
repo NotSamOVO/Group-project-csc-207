38cd2844cf2b5200e0996fe01d3b59402bd60ecd
vietng04 <viethoang.nguyen@mail.utoronto.ca>
2024-11-27 16:32:55 -0500

    update MatchResultsViewModel

--- src/main/java/interface_adapter/matchresults/MatchResultsViewModel.java
+++ src/main/java/interface_adapter/matchresults/MatchResultsViewModel.java
@@ -1,2 +1,19 @@
-package interface_adapter.matchresults;public class MatchResultsViewModel {
+package interface_adapter.matchresults;
+
+public class MatchResultsViewModel {
+    public static final String TEAM_NAME_LABEL = "Enter Team Name:";
+    public static final String NO_RESULTS_LABEL = "Results will appear here...";
+    public static final String GAME_LABEL = "Game";
+    public static final String SCORES_LABEL = "Results";
+    public static final String DATE_LABEL = "Date";
+    public static final String Q1_LABEL = "Q1";
+    public static final String Q2_LABEL = "Q2";
+    public static final String Q3_LABEL = "Q3";
+    public static final String Q4_LABEL = "Q4";
+    public static final String OT_LABEL = "OT";
+    public static final String VENUE_LABEL = "Venue";
+
+    public static final String SUBMIT_BUTTON_LABEL = "Submit";
+    public static final String SEARCH_BUTTON_LABEL = "Search";
+
 }

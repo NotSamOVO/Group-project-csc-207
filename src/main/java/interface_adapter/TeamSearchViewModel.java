9497a352c780148ea8c9dbc7edd4c2e2b82d893a
vietng04 <viethoang.nguyen@mail.utoronto.ca>
2024-11-27 16:32:35 -0500

    update TeamSearchViewModel

--- src/main/java/interface_adapter/TeamSearchViewModel.java
+++ src/main/java/interface_adapter/TeamSearchViewModel.java
@@ -4,13 +4,13 @@ package interface_adapter;
  * Model for the View Manager. Its state is the name of the View which
  * is currently active. An initial state of "" is used.
  */
-public class ViewManagerModel extends ViewModel<String> {
+public class TeamSearchViewModel extends ViewModel<String> {
 
-    private static String SEARCH_LABEL = "Search";
-    private static String TEAM_NAME_LABEL = "Please enter a team name!";
-    private static String ENTER_TEAM_NAME_LABEL = "Enter Team Name:";
+    public static String SEARCH_LABEL = "Search";
+    public static String TEAM_NAME_LABEL = "Please enter a team name!";
+    public static String ENTER_TEAM_NAME_LABEL = "Enter Team Name:";
 
-    public ViewManagerModel() {
+    public TeamSearchViewModel() {
         super("view manager");
         this.setState("");
     }

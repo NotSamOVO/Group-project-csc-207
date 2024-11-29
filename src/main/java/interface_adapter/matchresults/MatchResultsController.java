a828902f6e89bbceb3872345015920ab09ebff8f
vietng04 <viethoang.nguyen@mail.utoronto.ca>
2024-11-28 22:47:29 -0500

    update Controller

--- src/main/java/interface_adapter/matchresults/MatchResultsController.java
+++ src/main/java/interface_adapter/matchresults/MatchResultsController.java
@@ -1,2 +1,15 @@
-package interface_adapter.matchresults;public class MatchResultsController {
+package interface_adapter.matchresults;
+
+import use_case.matchresults.MatchResultsInputData;
+
+public class MatchResultsController {
+    private final MatchResultsInputData userMatchResults;
+
+    public MatchResultsController(MatchResultsInputData userMatchResults) {
+        this.userMatchResults = userMatchResults;
+    }
+
+    public String execute(String teamName) {
+        return userMatchResults.getTeamName();
+    }
 }

12efec863c4f113965ff1e6d9f11c22af08084dd
vietng04 <viethoang.nguyen@mail.utoronto.ca>
2024-12-01 16:40:55 -0500

    update TeamSearchController

--- src/main/java/interface_adapter/team_search/TeamSearchController.java
+++ src/main/java/interface_adapter/team_search/TeamSearchController.java
@@ -1,2 +1,22 @@
-package interface_adapter.team_search;public class TeamSearchController {
+package interface_adapter.team_search;
+
+import use_case.teamsearch.TeamSearchInputData;
+import use_case.teamsearch.TeamSearchUseCase;
+
+public class TeamSearchController {
+    TeamSearchUseCase teamSearchUseCase;
+
+    public TeamSearchController(TeamSearchUseCase teamSearchUseCase) {
+        this.teamSearchUseCase = teamSearchUseCase;
+    }
+
+    public boolean searchTeam(String teamName) {
+        String result = teamSearchUseCase.getTeamId(teamName);
+        TeamSearchInputData inputData = new TeamSearchInputData(result);
+
+        if (inputData.getTeamName().equals("Team Not Found")) {
+            return false;
+        }
+        return true;
+    }
 }

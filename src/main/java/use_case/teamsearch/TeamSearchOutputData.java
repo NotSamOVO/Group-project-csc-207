4f20d68c41563cd0aa156b0b55108c5f4fe95a97
vietng04 <viethoang.nguyen@mail.utoronto.ca>
2024-12-01 16:41:52 -0500

    update TeamSearchOutputData

--- src/main/java/use_case/teamsearch/TeamSearchOutputData.java
+++ src/main/java/use_case/teamsearch/TeamSearchOutputData.java
@@ -1,2 +1,15 @@
-package use_case.teamsearch;public class TeamSearchOutputData {
+package use_case.teamsearch;
+
+public class TeamSearchOutputData {
+    private final String message;
+
+    public TeamSearchOutputData(String message) {
+        this.message = message;
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
 }

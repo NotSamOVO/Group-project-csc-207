950f2bcef9bae68c199dc32572fe3a8af11e9798
vietng04 <viethoang.nguyen@mail.utoronto.ca>
2024-12-07 08:43:12 -0500

    update TeamSearchUseCase

--- src/main/java/use_case/teamsearch/TeamSearchUseCase.java
+++ src/main/java/use_case/teamsearch/TeamSearchUseCase.java
@@ -1,26 +1,32 @@
 package use_case.teamsearch;
 
 import api.NFLDataBase;
-import api.NFLTeamDataBase;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import entity.Team;
-import entity.Player;
-
 import java.util.ArrayList;
 
+/**
+ * This class handles the use case of searching for a team in the NFL database.
+ * It allows retrieving a team's details based on its name.
+ */
 public class TeamSearchUseCase {
     private NFLDataBase dataBase;
 
+    /**
+     * Constructs a TeamSearchUseCase object with the specified NFL database.
+     *
+     * @param dataBase The NFL database that contains the teams' data.
+     */
     public TeamSearchUseCase(NFLDataBase dataBase) {
         this.dataBase = dataBase;
     }
 
     /**
-     * get the team id by inputting team name.
-     * @param teamName The name of the team.
-     * @return "Team Not Found" if the team is not found.
+     * Retrieves the team details (in string format) by inputting the team name.
+     * Searches through the list of all teams in the database and returns the team details
+     * if the team is found. If no team is found with the given name, returns a "Team Not Found" message.
+     *
+     * @param teamName The name of the team to search for.
+     * @return A string representation of the team if found, or "Team Not Found" if the team does not exist.
      */
     public String getTeamId(String teamName) {
         ArrayList<Team> allTeams = dataBase.getAllTeams();
